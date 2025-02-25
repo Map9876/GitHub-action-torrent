4a765507bf3d2e28405e2f0222e15710bbbc841a
Your Name <you@example.com>
2025-02-26 03:36:49 +0800

    7

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 dpkg -i cloudflared-linux-amd64.deb
+HUGGINGFACE_TOKEN=$1
 
 # Start the Python server
-python3 server.py &
+python3 server.py $HUGGINGFACE_TOKEN &
